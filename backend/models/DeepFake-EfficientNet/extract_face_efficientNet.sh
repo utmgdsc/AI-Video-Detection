58b8834f0b010284424c9710b50406023fbed6ce
Marvin Wu/Wu Hung Mao <hongmao.wu@mail.utoronto.ca>
2026-02-01 15:07:47 -0500

    [whm] add wrapper extract_face_efficientNet script which automate extracting faces for AIGVDBench

--- backend/models/DeepFake-EfficientNet/extract_face_efficientNet.sh
+++ backend/models/DeepFake-EfficientNet/extract_face_efficientNet.sh
@@ -8,12 +8,12 @@ for SPLIT in "${SPLITS[@]}"; do
         echo "----------------------------------------------------"
         echo "Processing: $SPLIT / $CAT"
         echo "----------------------------------------------------"
-        
+
         # Check if the input directory actually exists and has files
-        if [ -d "/home/gdgteam1/AI-Video-Detection/backend/dataset/AIGVDBench/AIGVDBench/dataset_final/$SPLIT/$CAT" ]; then
+        if [ -d "/home/gdgteam1/AI-Video-Detection/backend/dataset/AIGVDBench/AIGVDBench/split_dataset/dataset_easyanimate_video_not_in_train_val/$SPLIT/$CAT" ]; then
             echo "Start extracting"
             python3 scripts/extract_faces.py \
-              --input-dir "/home/gdgteam1/AI-Video-Detection/backend/dataset/AIGVDBench/AIGVDBench/dataset_final/$SPLIT/$CAT" \
+              --input-dir "/home/gdgteam1/AI-Video-Detection/backend/dataset/AIGVDBench/AIGVDBench/split_dataset/dataset_easyanimate_video_not_in_train_val/$SPLIT/$CAT" \
               --output-dir "ff_extracted/$SPLIT/$CAT" \
               --mode video \
               --batch-size 60 \
