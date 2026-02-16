2547db78bc71a11693f8e338012476b581324df0
Frank Bi <frank.bi@mail.utoronto.ca>
2026-02-16 15:58:06 -0500

    SCRUM-23 Updated bash scripts and docs

--- backend/models/MesoNet/predict_standard_test.sh
+++ backend/models/MesoNet/predict_standard_test.sh
@@ -7,8 +7,3 @@ nohup python3 -u make-video-predictions.py Meso4_DF.h5 real 1.0 > logs/log_real.
 nohup python3 -u make-video-predictions.py Meso4_DF.h5 i2v 0.0 > logs/log_i2v.txt 2>&1 &
 nohup python3 -u make-video-predictions.py Meso4_DF.h5 t2v 0.0 > logs/log_t2v.txt 2>&1 &
 nohup python3 -u make-video-predictions.py Meso4_DF.h5 v2v 0.0 > logs/log_v2v.txt 2>&1 &
-
-# These processes run with the given directory '../../../dataset/FaceForensics++ Dataset'
-# nohup python3 -u make-video-predictions.py Meso4_DF.h5 Deepfakes 0.0 '../../dataset/FaceForensics++ Dataset' > logs/log_Deepfakes.txt 2>&1 &
-# nohup python3 -u make-video-predictions.py Meso4_DF.h5 Face2Face 0.0 '../../dataset/FaceForensics++ Dataset' > logs/log_Face2Face.txt 2>&1 &
-# nohup python3 -u make-video-predictions.py Meso4_DF.h5 original 1.0 '../../dataset/FaceForensics++ Dataset' > logs/log_FF_Original.txt 2>&1 &
