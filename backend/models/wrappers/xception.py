999a0d4cb882b19a2aacb7030f7a0aad31a4c5d1
MarcoBosss <marco20041115@icloud.com>
2026-02-23 00:44:20 -0500

    SCRUM-42 implemented all functions, ready for test

--- backend/models/wrappers/xception.py
+++ backend/models/wrappers/xception.py
@@ -1,7 +1,7 @@
 """
 XceptionNet model for deepfake detection.
 
-Team member: [YOUR NAME]
+Team member: Lin Wei/Marco Lin
 Docs: docs/models/xception/
 """
 
@@ -10,16 +10,17 @@ import torch
 import torch.nn as nn
 from pathlib import Path
 
-model_path = (Path(__file__).resolve().parents[3]
+path = (Path(__file__).resolve().parents[3]
               / "backend/models/XceptionNet-Detector/Deepfake-Detection")
-sys.path.append(str(model_path))
+sys.path.append(str(path))
+from network.models import model_selection
 from detect_from_video import predict_with_model
 
 # TODO: Implement model loading
 # Reference your docs/models/xception/02-source-and-setup.md for setup instructions
 
 
-def load_model(weights_path=None):
+def load_model(weights_path=None, cuda=True):
     """
     Load XceptionNet model.
 
@@ -30,13 +31,15 @@ def load_model(weights_path=None):
     Returns:
         model: Loaded PyTorch model ready for inference.
     """
-    # TODO: Implement
-    # Example:
-    # model = XceptionNet()
-    # if weights_path:
-    #     model.load_state_dict(torch.load(weights_path))
-    # return model
-    raise NotImplementedError("Implement load_model() - see docs/models/xception/")
+    model = model_selection(modelname='xception', num_out_classes=2, dropout=0.5)
+    if weights_path:
+        model.load_state_dict(torch.load(weights_path))
+    if isinstance(model, torch.nn.DataParallel):
+        model = model.module
+    if cuda:
+        model = model.cuda()
+    model.eval()
+    return model
 
 
 def predict(model, image):
