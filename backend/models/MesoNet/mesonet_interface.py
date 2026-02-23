d3adaaddc0e0a125eb7d372d72e77d3a66805f50
Frank Bi <frank.bi@mail.utoronto.ca>
2026-02-23 15:47:16 -0500

    SCRUM-43 Added MesoNet APIs for testing, requires integration with handlers

---
+++ backend/models/MesoNet/mesonet_interface.py
@@ -0,0 +1,141 @@
+"""
+Contains a class that mimics the functionalities of a PyTorch model.
+
+3.6 packages: TODO
+pip install fastapi==0.63.0 uvicorn==0.13.4
+
+3.10 packages: TODO
+pip install requests
+uvicorn mesonet_server:app --host 127.0.0.1 --port 8000
+"""
+
+import subprocess
+import requests
+import numpy as np
+import time
+
+BASE_URL = "http://"  # + "127.0.0.1:8000" to form complete URL
+DEFAULT_HOST = "127.0.0.1"
+DEFAULT_PORT = 8000
+ENV_PATH = "$HOME/miniconda3/envs/mesonet/bin/python3"  # TODO: Path to Conda python.exe. Should not use abs path, but for now
+DEFAULT_ARCHITECTURE = "Meso4"
+
+# TODO: Parse through
+
+
+class MesoNetClient:
+
+    def __init__(self):
+        self.url = BASE_URL + f"{DEFAULT_HOST}" + ":" + f"{DEFAULT_PORT}"
+        self.host = DEFAULT_HOST
+        self.port = DEFAULT_PORT
+        self.env_path = ENV_PATH
+        self.server_process = None
+        self.server_log = None
+
+        self.ensure_server_running()
+
+    def ensure_server_running(self):
+        try:
+            response = requests.get(self.url + "/test_server", timeout=1)
+            assert response.json()["success"], "Server is not running."
+        except:
+            print("Starting MesoNet server...")
+            self.start_server()
+            self.wait_until_ready()
+
+    def start_server(self, save_log=False):
+        output = subprocess.DEVNULL
+        if save_log:
+            self.server_log = open("logs/meso_server.txt", "a")
+            output = self.server_log
+
+        self.server_process = subprocess.Popen(
+            [
+                self.env_path,
+                "-u",
+                "-m", "uvicorn",
+                "mesonet_server:app",
+                "--host", f"{self.host}",
+                "--port", f"{self.port}"
+            ],
+            stdout=output,
+            stderr=output
+        )
+
+    def wait_until_ready(self):
+        for _ in range(20):
+            try:
+                response = requests.get(self.url + "/test_server", timeout=1)
+                assert response.json()["success"], "Server is not running."
+                print("Server ready.")
+                return
+            except:
+                time.sleep(0.5)
+
+        raise RuntimeError("Server failed to start")
+
+    def stop_server(self):
+        if self.server_process:
+            self.server_process.terminate()
+        if self.server_log is not None and not self.server_log.closed:
+            self.server_log.close()
+    
+    def __exit__(self, exc_type, exc, tb):
+        self.stop_server()
+
+    # =============== SERVER COMMUNICATION
+
+    def load_model(self, weights_path, model_config=None):
+
+        if model_config is not None:
+            architecture = model_config["architecture"]
+        else:
+            architecture = DEFAULT_ARCHITECTURE
+
+        response = requests.post(
+            self.url + "/load_model",
+            json={
+                "architecture": architecture,
+                "weights_path": weights_path
+            }
+        )
+        if response.json()["success"]:
+            return self
+        # Else model failed to load
+        return None
+
+    def process(self, faces, model_cfg, stop_server=True):
+        """
+        Analyze faces for deepfake detection.
+
+        Args:
+            faces: List of face images (cropped from video frames)
+            model_cfg: Dictionary of parameters defined in ensemble.yaml for this model
+
+        Returns: TODO: Change the return type? Or format correctly?
+            dict: {
+                'score': float (0-1, higher = more likely fake),
+                'per_frame_scores': list of floats,
+                'details': str
+            }
+        """
+        # TODO: Refactor load_model to take only model config?
+        self.load_model(model_cfg["weights_path"], model_config=model_cfg)
+
+        # Save faces to npy file
+        np.save("temp/faces.npy", faces)
+
+        # Send npy file path
+        response = requests.post(
+            self.url + "/process",
+            json={"faces_path": "temp/faces.npy"}
+        )
+        data = response.json()
+
+        if stop_server:
+            self.stop_server()
+        
+        if not data["success"]:
+            return {}
+        return data["predictions"]
